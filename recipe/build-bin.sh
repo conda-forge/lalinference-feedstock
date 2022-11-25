3a2ef8756f691584b877791a83a810528fb54c82
duncan.macleod <duncan.macleod@ligo.org>
2022-11-25 11:33:18 +0000

    add common build instructions script
    to reduce duplication/errors from copying between the various build scripts

--- recipe/build-bin.sh
+++ recipe/build-bin.sh
@@ -2,46 +2,28 @@
 
 set -ex
 
-_make="make -j ${CPU_COUNT} V=1 VERBOSE=1"
+# load common options
+. ${RECIPE_DIR}/common.sh
 
 # build in a sub-directory using a copy of the python build
 _builddir="_build${PY_VER}_${mpi}"
 cp -r _build${PY_VER} ${_builddir}
 cd ${_builddir}
 
-# replace package name in debug-prefix-map with source name
-export CFLAGS=$(
-   echo ${CFLAGS:-} |
-   sed -E 's|'\/usr\/local\/src\/conda\/${PKG_NAME}'|/usr/local/src/conda/lalinference|g'
-)
-
-# only link libraries we actually use
-export GSL_LIBS="-L${PREFIX}/lib -lgsl"
-
-# handle cross compiling
-if [[ $build_platform != $target_platform ]]; then
-	# help2man doesn't work when cross compiling
-	CONFIGURE_ARGS="${CONFIGURE_ARGS} --disable-help2man"
-
-	# enable cross compiling with openmpi
-	if [[ "${mpi}" == "openmpi" ]]; then
-		cp -rf ${PREFIX}/share/openmpi/*.txt ${BUILD_PREFIX}/share/openmpi/
-	fi
+# enable cross compiling with openmpi
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" && "${mpi}" == "openmpi" ]]; then
+  cp -rf ${PREFIX}/share/openmpi/*.txt ${BUILD_PREFIX}/share/openmpi/
 fi
 
 if [[ "${mpi}" != "nompi" ]]; then
-	# only the MPI executables use OpenMP
-	CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-mpi --enable-openmp"
+  CONFIGURE_ARGS="${CONFIGURE_ARGS} --enable-mpi"
 fi
 
 # configure only what we need to build executables (and man pages)
 ${SRC_DIR}/configure \
-	--disable-doxygen \
-	--disable-gcc-flags \
-	--disable-swig \
-	--enable-python \
-	--prefix=$PREFIX \
-	${CONFIGURE_ARGS} \
+  ${CONFIGURE_ARGS} \
+  --disable-swig \
+  --enable-python \
 ;
 
 # install binaries
